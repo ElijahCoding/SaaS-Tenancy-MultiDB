86e6f33d15fbfa5919a88e43dfe9cffd581751df
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-06 13:45:20 +0300

    uuid built

--- app/Tenant/Traits/IsTenant.php
+++ app/Tenant/Traits/IsTenant.php
@@ -4,12 +4,17 @@ namespace App\Tenant\Traits;
 
 use App\TenantConnection;
 use App\Tenant\Models\Tenant;
+use Webpatser\Uuid\Uuid;
 
 trait IsTenant
 {
     public static function boot()
     {
         parent::boot();
+
+        static::creating(function ($tenant) {
+            $tenant->uuid = Uuid::generate(4);
+        });
 
         static::created(function ($tenant) {
             $tenant->tenantConnection()->save(
