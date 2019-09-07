9b8a54ec96411a6560149a91f38bc876f13d17a5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-07 16:53:33 +0300

    listing companies , view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Http\ViewComposers\NavigationViewComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('layouts.partials._nav', NavigationViewComposer::class);
     }
 
     /**
