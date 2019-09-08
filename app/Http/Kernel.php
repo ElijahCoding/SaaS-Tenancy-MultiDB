234c93245c07f8ca1448e980c43ef55d66f5c97f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-08 14:51:56 +0300

    tenant identification middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,12 +34,17 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
+        ],
+
+        'tenant' => [
+            \App\Http\Middleware\Tenant\SetTenant::class,
+            'bindings'
         ],
     ];
 
